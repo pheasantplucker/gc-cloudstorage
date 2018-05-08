dfadae223393dca16c2a2f188021b9eb3f28cca0
chrisglick <chrisglick@gmail.com>
2018-05-07 17:28:00 -0700

    Added some validation function and bucketExists. Test failing is because of no cleanup yet

--- src/cloudstorage.js
+++ src/cloudstorage.js
@@ -8,6 +8,13 @@ const {
 const ramda = require('ramda')
 const Storage = require('@google-cloud/storage')
 
+const noUpperCase = name => {
+  const nameRegex = new RegExp(/[A-Z]/)
+  const anyUpperCaseMatch = name.match(nameRegex)
+  if (anyUpperCaseMatch === null) return true
+  return false
+}
+
 let storage
 
 const createStorageClient = projectId => {
@@ -22,4 +29,47 @@ const createStorageClient = projectId => {
   }
 }
 
-module.exports = { createStorageClient }
+const createBucket = async bucketName => {
+  const validName = noUpperCase(bucketName)
+  if (!validName) {
+    return failure(bucketName, {
+      error:
+        'Bucketname contains upper case illegal characters. | a-z 0-9 - | only',
+    })
+  }
+  try {
+    const result = await storage.createBucket(bucketName)
+    const response = result[0]
+    if (bucketName !== response.id) {
+      return failure('Did not create the bucket', response)
+    }
+    return success(result, response)
+  } catch (e) {
+    return failure(e.toString())
+  }
+}
+
+const bucketExists = async bucketName => {
+  const validName = noUpperCase(bucketName)
+  if (!validName) {
+    return failure(bucketName, {
+      error:
+        'Bucketname contains upper case, or illegal characters. | a-z 0-9 - | only',
+    })
+  }
+  const bucket = storage.bucket(bucketName)
+
+  const exists = await bucket.exists()
+  if (exists) {
+    return success(exists)
+  }
+
+  return failure(exists, { bucketName: bucketName, bucket: bucket })
+}
+
+module.exports = {
+  createStorageClient,
+  createBucket,
+  bucketExists,
+  noUpperCase,
+}
--- src/cloudstorage.spec.js
+++ src/cloudstorage.spec.js
@@ -8,7 +8,12 @@ const {
 } = require(`@pheasantplucker/failables`)
 const assert = require('assert')
 const equal = assert.deepEqual
-const { createStorageClient } = require('./cloudstorage')
+const {
+  createStorageClient,
+  createBucket,
+  bucketExists,
+  noUpperCase,
+} = require('./cloudstorage')
 
 const { GC_PROJECT_ID } = process.env
 
@@ -16,5 +21,44 @@ describe(`createStorageClient()`, () => {
   it(`should return a client`, () => {
     const result = createStorageClient(GC_PROJECT_ID)
     assertSuccess(result)
+  })
+})
+
+const bucketName = 'testbucketter124321433'
+
+describe('createBucket()', () => {
+  it('should create a bucket IF IT DOES NOT EXIST', async () => {
+    const result = await createBucket(bucketName)
+    assertSuccess(result)
+  })
+  it('should return a failure if the name is illegal', async () => {
+    const result = await createBucket('noUppercasecharactersAllowed')
+    assertFailure(result)
+  })
+})
+
+describe(`bucketExists()`, () => {
+  it(`should return TRUE if a bucket exists`, async () => {
+    const result = await bucketExists(bucketName)
+    assertSuccess(result)
+  })
+  it(`should return False if a bucket doesnt exist`, async () => {
+    const result = await bucketExists(
+      'awduhniou32hbruihnb12319u5hb1tbR3198trb723287rb325125jj'
+    )
+    // someone will make the above bucket and THEN PROBLEMS BEGIN
+    // replace w/ guid?
+    assertFailure(result)
+  })
+})
+
+describe(`noUpperCase()`, () => {
+  it(`should return true if there are no upper case, false otherwise`, () => {
+    const noUpper = 'adawfgnuiognnuni0nnqnnwdqd'
+    const withUpper = 'adawfgnuiognnuniOnnqnnwdqd'
+    const result = noUpperCase(noUpper)
+    equal(result, true)
+    const result2 = noUpperCase(withUpper)
+    equal(result2, false)
   })
 })
