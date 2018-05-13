dfb7f194c90e59fb0f445be6d5bca94effe202eb
Tim Cash <timcash@gmail.com>
2018-05-13 12:58:00 -0600

    adding file API

--- src/cloudstorage.js
+++ src/cloudstorage.js
@@ -2,10 +2,10 @@ const {
   failure,
   success,
   payload,
-  // isFailure,
+  isFailure,
 } = require('@pheasantplucker/failables-node6')
 // const ramda = require('ramda')
-const Storage = require('@google-cloud/storage')
+const storage = require('@google-cloud/storage')()
 
 const noUpperCase = name => {
   if (name === '' || name === undefined) return false
@@ -13,20 +13,6 @@ const noUpperCase = name => {
   const anyUpperCaseMatch = name.match(nameRegex)
   if (anyUpperCaseMatch === null) return true
   return false
-}
-
-let storage
-
-const createStorageClient = projectId => {
-  try {
-    const newStorage = new Storage({
-      projectId: projectId,
-    })
-    storage = newStorage
-    return success(newStorage)
-  } catch (e) {
-    return failure(e.toString())
-  }
 }
 
 const createBucket = async bucketName => {
@@ -68,6 +54,56 @@ const bucketExists = async bucketName => {
   return failure(exists, { bucketName: bucketName, bucket: bucket })
 }
 
+async function exists(filename) {
+  const { bucketpart, filepart } = split_filename(filename)
+  try {
+    // first check that the bucket exists
+    const r1 = bucketExists(bucketpart)
+    if(isFailure(r1)) return r1
+    if(payload(r1) === false) return success(false)
+    const fileHandle = getFileHandle(filename)
+    const result = await fileHandle.exists()
+    return success(result[0])
+  } catch (e) {
+    console.log(e.toString())
+    return failure(e.toString())
+  }
+}
+async function save(filename, data) {
+  try {
+    const fileHandle = getFileHandle(filename)
+    const result = await fileHandle.save(data)
+    return success(result)
+  } catch (e) {
+    console.log(e.toString())
+    return failure(e.toString())
+  }
+}
+
+async function getReadStream(filename, opts) {
+  try {
+    const fileHandle = getFileHandle(filename)
+    const result = fileHandle.createReadStream()
+    return success(result)
+  } catch (e) {
+    console.log(e.toString())
+    return failure(e.toString())
+  }
+}
+
+function getFileHandle(filepath) {
+  const { bucketpart, filepart } = split_filename(filepath)
+  const bucket = storage.bucket(bucketpart)
+  const file = bucket.file(filepart)
+  return file
+}
+
+function split_filename(n) {
+  const [bucketpart, ...filepartarray] = n.split("/")
+  const filepart = filepartarray.join("/")
+  return { bucketpart, filepart }
+}
+
 const uploadFile = async (bucketName, filePath) => {
   try {
     const getThisBucket = await getBucket(bucketName)
@@ -97,11 +133,13 @@ const newFile = (bucketName, filePath) => {
   return success(result)
 }
 module.exports = {
-  createStorageClient,
   createBucket,
   bucketExists,
   noUpperCase,
   uploadFile,
   getBucket,
   newFile,
+  exists,
+  save,
+  getReadStream
 }
--- src/cloudstorage.spec.js
+++ src/cloudstorage.spec.js
@@ -1,36 +1,31 @@
 const {
   assertSuccess,
   assertFailure,
-  // payload,
+  payload,
   // isSuccess,
   // isFailure,
 } = require(`@pheasantplucker/failables-node6`)
 const assert = require('assert')
 const equal = assert.deepEqual
 const {
-  createStorageClient,
   createBucket,
   getBucket,
   bucketExists,
   uploadFile,
   noUpperCase,
   newFile,
+  exists,
+  save,
+  getReadStream,
+  createWriteStream
 } = require('./cloudstorage')
 
 const uuid = require('uuid')
-
-const { GC_PROJECT_ID } = process.env
-
-describe(`createStorageClient()`, () => {
-  it(`should return a client`, () => {
-    const result = createStorageClient(GC_PROJECT_ID)
-    assertSuccess(result)
-  })
-})
-
 const bucketName = 'testbucketteer' + uuid.v4()
+const file_id = uuid.v4()
 
-describe('createBucket()', () => {
+describe('createBucket()', function () {
+  this.timeout(540 * 1000)
   it('should create a bucket IF IT DOES NOT EXIST', async () => {
     const result = await createBucket(bucketName)
     assertSuccess(result)
@@ -41,24 +36,97 @@ describe('createBucket()', () => {
   })
 })
 
-describe(`bucketExists()`, () => {
-  it(`should return TRUE if a bucket exists`, async () => {
-    const result = await bucketExists(bucketName)
-    assertSuccess(result)
-  })
-  it(`should return False if a bucket doesnt exist`, async () => {
-    const randomBucket = 'awduhniou32hbruitb' + uuid.v4()
-    const result = await bucketExists(randomBucket)
-    assertFailure(result)
+describe('save()', function () {
+  this.timeout(540 * 1000)
+  
+  it('should save a file', async () => {
+    const filepath = `${bucketName}/${file_id}.txt`
+    const data = 'foobar'
+    const r1 = await save(filepath, data)
+    assertSuccess(r1)
+    const r2 = await exists(filepath)
+    assertSuccess(r2, true)
   })
 })
 
-describe(`getBucket(bucketName)`, () => {
-  it(`should return a bucketObj`, () => {
-    const result = getBucket(bucketName)
-    assertSuccess(result)
+describe('exists()', function () {
+  this.timeout(540 * 1000)
+  it('should check that a file exists', async () => {
+    const filepath = `${bucketName}/${file_id}.txt`
+    const result = await exists(filepath)
+    assertSuccess(result, true)
+  })
+  it('should return false if the file does not exist', async () => {
+    const bucket_that_does_not_exist = uuid.v4()
+    const filepath = `${bucket_that_does_not_exist}/${file_id}.txt`
+    const result = await exists(filepath)
+    assertSuccess(result, false)
   })
 })
+
+describe('getReadStream()', function () {
+  this.timeout(540 * 1000)
+  it('should return a read stream', async () => {
+    const filepath = `${bucketName}/${file_id}.txt`
+    const opts = {}
+    const result = await getReadStream(filepath, opts)
+    assertSuccess(result)
+    const stream = payload(result)
+
+    return new Promise((resolve) => {
+      let buffer = ''
+      stream.on('data', (chunk)=> {
+        buffer += chunk
+      })
+      stream.on('end', ()=> {
+        equal('foobar', buffer)
+        resolve()
+      })
+    })
+  })
+})
+
+describe('createWriteStream()', function () {
+  this.timeout(540 * 1000)
+  it('should write data to a file', async () => {
+    const filepath = `${bucketName}/${file_id}.txt`
+    const opts = {}
+    const result = await createWriteStream(filepath, opts)
+    assertSuccess(result)
+    const stream = payload(result)
+
+    return new Promise((resolve) => {
+      let buffer = ''
+      stream.on('data', (chunk)=> {
+        buffer += chunk
+      })
+      stream.on('end', ()=> {
+        equal('foobar', buffer)
+        resolve()
+      })
+    })
+  })
+})
+
+
+// describe.skip(`bucketExists()`, () => {
+//   it(`should return TRUE if a bucket exists`, async () => {
+//     const result = await bucketExists(bucketName)
+//     assertSuccess(result)
+//   })
+//   it(`should return False if a bucket doesnt exist`, async () => {
+//     const randomBucket = 'awduhniou32hbruitb' + uuid.v4()
+//     const result = await bucketExists(randomBucket)
+//     assertFailure(result)
+//   })
+// })
+
+// describe.skip(`getBucket(bucketName)`, () => {
+//   it(`should return a bucketObj`, () => {
+//     const result = getBucket(bucketName)
+//     assertSuccess(result)
+//   })
+// })
 // describe(`deleteFiles(query)`, () => {
 // it(`should delete the file in question`, () => {
 // const result = deleteFiles(query )
@@ -66,29 +134,29 @@ describe(`getBucket(bucketName)`, () => {
 // })
 // })
 
-describe(`newFile()`, () => {
-  it(`should return a file object with the path`, () => {
-    const fileTest = 'c:/test.txt'
-    const result = newFile(bucketName, fileTest)
-    assertSuccess(result)
-  })
-})
+// describe.skip(`newFile()`, () => {
+//   it(`should return a file object with the path`, () => {
+//     const fileTest = 'c:/test.txt'
+//     const result = newFile(bucketName, fileTest)
+//     assertSuccess(result)
+//   })
+// })
 
-describe(`uploadFile()`, () => {
-  it(`should upload the test file`, async () => {
-    const testFile = './test/newFile.txt'
-    const result = await uploadFile(bucketName, testFile) //, options, callback
-    assertSuccess(result)
-  })
-})
+// describe.skip(`uploadFile()`, () => {
+//   it(`should upload the test file`, async () => {
+//     const testFile = './test/newFile.txt'
+//     const result = await uploadFile(bucketName, testFile) //, options, callback
+//     assertSuccess(result)
+//   })
+// })
 
-describe(`noUpperCase()`, () => {
-  it(`should return true if there are no upper case, false otherwise`, () => {
-    const noUpper = 'adawfgnuiognnuni0nnqnnwdqd'
-    const withUpper = 'adawfgnuiognnuniOnnqnnwdqd'
-    const result = noUpperCase(noUpper)
-    equal(result, true)
-    const result2 = noUpperCase(withUpper)
-    equal(result2, false)
-  })
-})
+// describe.skip(`noUpperCase()`, () => {
+//   it(`should return true if there are no upper case, false otherwise`, () => {
+//     const noUpper = 'adawfgnuiognnuni0nnqnnwdqd'
+//     const withUpper = 'adawfgnuiognnuniOnnqnnwdqd'
+//     const result = noUpperCase(noUpper)
+//     equal(result, true)
+//     const result2 = noUpperCase(withUpper)
+//     equal(result2, false)
+//   })
+// })
